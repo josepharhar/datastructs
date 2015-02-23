71b1ebca5e3acd99b3f0b9f747c601a4586d0b93
Joseph Arhar <josepharhar@gmail.com>
2015-02-22 23:39:09 -0800

    Mocking!

--- src/tests/ArrayListTest.java
+++ src/tests/ArrayListTest.java
@@ -1,15 +1,41 @@
 package tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 import lists.ArrayList;
 
 import org.junit.Test;
-import org.mockito.Mock;
+
+import org.mockito.Mockito.*;
+
+import util.StringHolder;
 
 public class ArrayListTest {
     
-    @Mock
-    ArrayList<Integer> mockList;
+//    @Mock
+//    StringHolder strHolder;
+    
+	@Test
+	public void strMockTest() {
+		StringHolder test = mock(StringHolder.class);
+
+		when(test.getString()).thenReturn("this is a mock");
+		
+		System.out.println("test.getString(): " + test.getString());
+		
+//		test.when(test.getString()).thenReturn("this is a mock");
+	}
+	
+	@Test
+	public void getStringTest() {
+		StringHolder test = mock(StringHolder.class);
+		
+		test.setString("this is a new string");
+		test.getString();
+		
+		verify(test).setString("this is a new stringasdf");
+		verify(test).getString();
+	}
 
     @Test
     public void addGetTenIntsTest() {
---
+++ src/util/StringHolder.java
@@ -0,0 +1,17 @@
+package util;
+
+public class StringHolder {
+	private String string;
+	
+	public StringHolder(String string) {
+		this.string = string;
+	}
+	
+	public String getString() {
+		return string;
+	}
+	
+	public void setString(String string) {
+		this.string = string;
+	}
+}
